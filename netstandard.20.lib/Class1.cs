c634d75322ce687ce19ae8c086d1c812e1ead817
salaros <salaros@yandex.ru>
2017-05-15 02:02:30 +0300

    implementing a basic PoC

---
+++ netstandard.20.lib/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace netstandard.v2_0.lib
+{
+    public class MyFancyUtil
+    {
+        public static readonly Dictionary<string, string> SimplierThanThis;
+
+        static MyFancyUtil()
+        {
+            SimplierThanThis = new Dictionary<string, string>
+            {
+                { "foo", "bar" }
+            };
+        }
+    }
+}
--- netstandard.20.test/Program.cs
+++ netstandard.20.test/Program.cs
@@ -1,15 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using netstandard.v2_0.lib;
 
-namespace netstandard._20.test
+namespace netstandard.v2_0.test
 {
     class Program
     {
         static void Main(string[] args)
         {
+            foreach (var s in MyFancyUtil.SimplierThanThis)
+            {
+                s.ToString();
+            }
         }
     }
 }
